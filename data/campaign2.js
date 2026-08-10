f3c8c3cb625cd4a16c9613fe5b85d2846292f426
madipearl36 <madison.pearl0306@gmail.com>
2026-08-09 20:38:19 -0400

    Added Arc 2 to db

--- data/campaign2.js
+++ data/campaign2.js
@@ -222,6 +222,204 @@ const campaign2Episodes = [
         episode: 25,
         label: "Campaign 2 • Episode 25",
         title: "Divergent Paths"
+      },
+
+      {
+        id: "c2e26",
+        campaign: 2,
+        arc: 2,
+        episode: 26,
+        label: "Campaign 2 • Episode 26",
+        title: "Found & Lost"
+      },
+      
+      {
+        id: "c2e27",
+        campaign: 2,
+        arc: 2,
+        episode: 27,
+        label: "Campaign 2 • Episode 27",
+        title: "Converging Fury"
+      },
+      
+      {
+        id: "c2e28",
+        campaign: 2,
+        arc: 2,
+        episode: 28,
+        label: "Campaign 2 • Episode 28",
+        title: "Within the Nest"
+      },
+      
+      {
+        id: "c2e29",
+        campaign: 2,
+        arc: 2,
+        episode: 29,
+        label: "Campaign 2 • Episode 29",
+        title: "The Stalking Nightmare"
+      },
+      
+      {
+        id: "c2e30",
+        campaign: 2,
+        arc: 2,
+        episode: 30,
+        label: "Campaign 2 • Episode 30",
+        title: "The Journey Home"
+      },
+      
+      {
+        id: "c2e31",
+        campaign: 2,
+        arc: 2,
+        episode: 31,
+        label: "Campaign 2 • Episode 31",
+        title: "Commerce & Chaos"
+      },
+      
+      {
+        id: "c2e32",
+        campaign: 2,
+        arc: 2,
+        episode: 32,
+        label: "Campaign 2 • Episode 32",
+        title: "Beyond the Boundaries"
+      },
+      
+      {
+        id: "c2e33",
+        campaign: 2,
+        arc: 2,
+        episode: 33,
+        label: "Campaign 2 • Episode 33",
+        title: "The Ruby and the Sapphire"
+      },
+      
+      {
+        id: "c2e34",
+        campaign: 2,
+        arc: 2,
+        episode: 34,
+        label: "Campaign 2 • Episode 34",
+        title: "Encroaching Waters"
+      },
+      
+      {
+        id: "c2e35",
+        campaign: 2,
+        arc: 2,
+        episode: 35,
+        label: "Campaign 2 • Episode 35",
+        title: "Dockside Diplomacy"
+      },
+      
+      {
+        id: "c2e36",
+        campaign: 2,
+        arc: 2,
+        episode: 36,
+        label: "Campaign 2 • Episode 36",
+        title: "O Captain, Who's Captain?"
+      },
+      
+      {
+        id: "c2e37",
+        campaign: 2,
+        arc: 2,
+        episode: 37,
+        label: "Campaign 2 • Episode 37",
+        title: "Dangerous Liaisons"
+      },
+      
+      {
+        id: "c2e38",
+        campaign: 2,
+        arc: 2,
+        episode: 38,
+        label: "Campaign 2 • Episode 38",
+        title: "Welcome to the Jungle"
+      },
+      
+      {
+        id: "c2e39",
+        campaign: 2,
+        arc: 2,
+        episode: 39,
+        label: "Campaign 2 • Episode 39",
+        title: "Temple of the False Serpent"
+      },
+      
+      {
+        id: "c2e40",
+        campaign: 2,
+        arc: 2,
+        episode: 40,
+        label: "Campaign 2 • Episode 40",
+        title: "Dubious Pursuits"
+      },
+      
+      {
+        id: "c2e41",
+        campaign: 2,
+        arc: 2,
+        episode: 41,
+        label: "Campaign 2 • Episode 41",
+        title: "A Pirate's Life for Me"
+      },
+      
+      {
+        id: "c2e42",
+        campaign: 2,
+        arc: 2,
+        episode: 42,
+        label: "Campaign 2 • Episode 42",
+        title: "A Hole In the Plan"
+      },
+      
+      {
+        id: "c2e43",
+        campaign: 2,
+        arc: 2,
+        episode: 43,
+        label: "Campaign 2 • Episode 43",
+        title: "In Hot Water"
+      },
+      
+      {
+        id: "c2e44",
+        campaign: 2,
+        arc: 2,
+        episode: 44,
+        label: "Campaign 2 • Episode 44",
+        title: "The Diver's Grave"
+      },
+      
+      {
+        id: "c2e45",
+        campaign: 2,
+        arc: 2,
+        episode: 45,
+        label: "Campaign 2 • Episode 45",
+        title: "The Stowaway"
+      },
+      
+      {
+        id: "c2e46",
+        campaign: 2,
+        arc: 2,
+        episode: 46,
+        label: "Campaign 2 • Episode 46",
+        title: "A Storm of Memories"
+      },
+      
+      {
+        id: "c2e47",
+        campaign: 2,
+        arc: 2,
+        episode: 47,
+        label: "Campaign 2 • Episode 47",
+        title: "The Second Seal"
       }
 ];
 
