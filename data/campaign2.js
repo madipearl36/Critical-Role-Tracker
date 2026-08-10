35f80c3b4605ebf35d99939beb907739644840ec
madipearl36 <madison.pearl0306@gmail.com>
2026-08-09 22:19:32 -0400

    Finished adding campaign 2

--- data/campaign2.js
+++ data/campaign2.js
@@ -618,6 +618,654 @@ const campaign2Episodes = [
         episode: 69,
         label: "Campaign 2 • Episode 69",
         title: "The King's Cage"
+      },
+
+      {
+        id: "c2e70",
+        campaign: 2,
+        arc: 4,
+        episode: 70,
+        label: "Campaign 2 • Episode 70",
+        title: "Causatum"
+      },
+      
+      {
+        id: "c2e71",
+        campaign: 2,
+        arc: 4,
+        episode: 71,
+        label: "Campaign 2 • Episode 71",
+        title: "Family Gathering"
+      },
+      
+      {
+        id: "c2e72",
+        campaign: 2,
+        arc: 4,
+        episode: 72,
+        label: "Campaign 2 • Episode 72",
+        title: "Clay and Dust"
+      },
+      
+      {
+        id: "c2e73",
+        campaign: 2,
+        arc: 4,
+        episode: 73,
+        label: "Campaign 2 • Episode 73",
+        title: "Uthodurn"
+      },
+      
+      {
+        id: "c2e74",
+        campaign: 2,
+        arc: 4,
+        episode: 74,
+        label: "Campaign 2 • Episode 74",
+        title: "Manifold Morals"
+      },
+      
+      {
+        id: "c2e75",
+        campaign: 2,
+        arc: 4,
+        episode: 75,
+        label: "Campaign 2 • Episode 75",
+        title: "Rime and Reason"
+      },
+      
+      {
+        id: "c2e76",
+        campaign: 2,
+        arc: 4,
+        episode: 76,
+        label: "Campaign 2 • Episode 76",
+        title: "Refjorged"
+      },
+      
+      {
+        id: "c2e77",
+        campaign: 2,
+        arc: 4,
+        episode: 77,
+        label: "Campaign 2 • Episode 77",
+        title: "A Tangled Web"
+      },
+      
+      {
+        id: "c2e78",
+        campaign: 2,
+        arc: 4,
+        episode: 78,
+        label: "Campaign 2 • Episode 78",
+        title: "Between the Lines"
+      },
+      
+      {
+        id: "c2e79",
+        campaign: 2,
+        arc: 4,
+        episode: 79,
+        label: "Campaign 2 • Episode 79",
+        title: "Through the Trees"
+      },
+      
+      {
+        id: "c2e80",
+        campaign: 2,
+        arc: 4,
+        episode: 80,
+        label: "Campaign 2 • Episode 80",
+        title: "The Folding Halls"
+      },
+      
+      {
+        id: "c2e81",
+        campaign: 2,
+        arc: 4,
+        episode: 81,
+        label: "Campaign 2 • Episode 81",
+        title: "From Door to Door"
+      },
+      
+      {
+        id: "c2e82",
+        campaign: 2,
+        arc: 4,
+        episode: 82,
+        label: "Campaign 2 • Episode 82",
+        title: "The Beat of the Permaheart"
+      },
+      
+      {
+        id: "c2e83",
+        campaign: 2,
+        arc: 4,
+        episode: 83,
+        label: "Campaign 2 • Episode 83",
+        title: "Dark Bargains"
+      },
+      
+      {
+        id: "c2e84",
+        campaign: 2,
+        arc: 4,
+        episode: 84,
+        label: "Campaign 2 • Episode 84",
+        title: "Titles and Tattoos"
+      },
+      
+      {
+        id: "c2e85",
+        campaign: 2,
+        arc: 4,
+        episode: 85,
+        label: "Campaign 2 • Episode 85",
+        title: "The Threads Converge"
+      },
+      
+      {
+        id: "c2e86",
+        campaign: 2,
+        arc: 4,
+        episode: 86,
+        label: "Campaign 2 • Episode 86",
+        title: "The Cathedral"
+      },
+      
+      {
+        id: "c2e87",
+        campaign: 2,
+        arc: 4,
+        episode: 87,
+        label: "Campaign 2 • Episode 87",
+        title: "Punishment and Politics"
+      },
+      
+      {
+        id: "c2e88",
+        campaign: 2,
+        arc: 4,
+        episode: 88,
+        label: "Campaign 2 • Episode 88",
+        title: "Unwanted Reunions"
+      },
+      
+      {
+        id: "c2e89",
+        campaign: 2,
+        arc: 4,
+        episode: 89,
+        label: "Campaign 2 • Episode 89",
+        title: "Lingering Wounds"
+      },
+      
+      {
+        id: "c2e90",
+        campaign: 2,
+        arc: 4,
+        episode: 90,
+        label: "Campaign 2 • Episode 90",
+        title: "Bathhouses and Bastions"
+      },
+      
+      {
+        id: "c2e91",
+        campaign: 2,
+        arc: 4,
+        episode: 91,
+        label: "Campaign 2 • Episode 91",
+        title: "Stone to Clay"
+      },
+
+      {
+        id: "c2e92",
+        campaign: 2,
+        arc: 5,
+        episode: 92,
+        label: "Campaign 2 • Episode 92",
+        title: "Home is Where the Heart Is"
+      },
+      
+      {
+        id: "c2e93",
+        campaign: 2,
+        arc: 5,
+        episode: 93,
+        label: "Campaign 2 • Episode 93",
+        title: "Misery Loves Company"
+      },
+      
+      {
+        id: "c2e94",
+        campaign: 2,
+        arc: 5,
+        episode: 94,
+        label: "Campaign 2 • Episode 94",
+        title: "With Great Power..."
+      },
+      
+      {
+        id: "c2e95",
+        campaign: 2,
+        arc: 5,
+        episode: 95,
+        label: "Campaign 2 • Episode 95",
+        title: "Blessing in Disguise"
+      },
+      
+      {
+        id: "c2e96",
+        campaign: 2,
+        arc: 5,
+        episode: 96,
+        label: "Campaign 2 • Episode 96",
+        title: "Family Shatters"
+      },
+      
+      {
+        id: "c2e97",
+        campaign: 2,
+        arc: 5,
+        episode: 97,
+        label: "Campaign 2 • Episode 97",
+        title: "The Fancy and the Fooled"
+      },
+      
+      {
+        id: "c2e98",
+        campaign: 2,
+        arc: 5,
+        episode: 98,
+        label: "Campaign 2 • Episode 98",
+        title: "Dark Waters"
+      },
+      
+      {
+        id: "c2e99",
+        campaign: 2,
+        arc: 5,
+        episode: 99,
+        label: "Campaign 2 • Episode 99",
+        title: "High Seas, High Stakes"
+      },
+      
+      {
+        id: "c2e100",
+        campaign: 2,
+        arc: 5,
+        episode: 100,
+        label: "Campaign 2 • Episode 100",
+        title: "Hunted at Sea"
+      },
+      
+      {
+        id: "c2e101",
+        campaign: 2,
+        arc: 5,
+        episode: 101,
+        label: "Campaign 2 • Episode 101",
+        title: "Mysteries, Memories, and Music"
+      },
+      
+      {
+        id: "c2e102",
+        campaign: 2,
+        arc: 5,
+        episode: 102,
+        label: "Campaign 2 • Episode 102",
+        title: "Ghosts, Dinosaurs, and Stuff"
+      },
+      
+      {
+        id: "c2e103",
+        campaign: 2,
+        arc: 5,
+        episode: 103,
+        label: "Campaign 2 • Episode 103",
+        title: "Maritime Mysteries"
+      },
+      
+      {
+        id: "c2e104",
+        campaign: 2,
+        arc: 5,
+        episode: 104,
+        label: "Campaign 2 • Episode 104",
+        title: "The Ruined Sliver"
+      },
+      
+      {
+        id: "c2e105",
+        campaign: 2,
+        arc: 5,
+        episode: 105,
+        label: "Campaign 2 • Episode 105",
+        title: "Rumble at Rumblecusp"
+      },
+      
+      {
+        id: "c2e106",
+        campaign: 2,
+        arc: 5,
+        episode: 106,
+        label: "Campaign 2 • Episode 106",
+        title: "A Fog Lifted"
+      },
+      
+      {
+        id: "c2e107",
+        campaign: 2,
+        arc: 5,
+        episode: 107,
+        label: "Campaign 2 • Episode 107",
+        title: "Devoutness and Dicks"
+      },
+      
+      {
+        id: "c2e108",
+        campaign: 2,
+        arc: 5,
+        episode: 108,
+        label: "Campaign 2 • Episode 108",
+        title: "Traveler Con"
+      },
+      
+      {
+        id: "c2e109",
+        campaign: 2,
+        arc: 5,
+        episode: 109,
+        label: "Campaign 2 • Episode 109",
+        title: "Frigid Propositions"
+      },
+      
+      {
+        id: "c2e110",
+        campaign: 2,
+        arc: 5,
+        episode: 110,
+        label: "Campaign 2 • Episode 110",
+        title: "Dinner with the Devil"
+      },
+      
+      {
+        id: "c2e111",
+        campaign: 2,
+        arc: 5,
+        episode: 111,
+        label: "Campaign 2 • Episode 111",
+        title: "New Homes and Old Friends"
+      },
+      
+      {
+        id: "c2e112",
+        campaign: 2,
+        arc: 5,
+        episode: 112,
+        label: "Campaign 2 • Episode 112",
+        title: "The Chase Begins"
+      },
+
+      {
+        id: "c2e113",
+        campaign: 2,
+        arc: 6,
+        episode: 113,
+        label: "Campaign 2 • Episode 113",
+        title: "A Heart Grown Cold"
+      },
+      
+      {
+        id: "c2e114",
+        campaign: 2,
+        arc: 6,
+        episode: 114,
+        label: "Campaign 2 • Episode 114",
+        title: "An Open Window"
+      },
+      
+      {
+        id: "c2e115",
+        campaign: 2,
+        arc: 6,
+        episode: 115,
+        label: "Campaign 2 • Episode 115",
+        title: "Fetching Fables & Frosty Friends"
+      },
+      
+      {
+        id: "c2e116",
+        campaign: 2,
+        arc: 6,
+        episode: 116,
+        label: "Campaign 2 • Episode 116",
+        title: "Under Timeless Ice"
+      },
+      
+      {
+        id: "c2e117",
+        campaign: 2,
+        arc: 6,
+        episode: 117,
+        label: "Campaign 2 • Episode 117",
+        title: "The Tortoise and The Dare"
+      },
+      
+      {
+        id: "c2e118",
+        campaign: 2,
+        arc: 6,
+        episode: 118,
+        label: "Campaign 2 • Episode 118",
+        title: "Solace Between the Secrets"
+      },
+      
+      {
+        id: "c2e119",
+        campaign: 2,
+        arc: 6,
+        episode: 119,
+        label: "Campaign 2 • Episode 119",
+        title: "Malice and Mystery Below"
+      },
+      
+      {
+        id: "c2e120",
+        campaign: 2,
+        arc: 6,
+        episode: 120,
+        label: "Campaign 2 • Episode 120",
+        title: "Contentious Company"
+      },
+      
+      {
+        id: "c2e121",
+        campaign: 2,
+        arc: 6,
+        episode: 121,
+        label: "Campaign 2 • Episode 121",
+        title: "Ice and Fire"
+      },
+      
+      {
+        id: "c2e122",
+        campaign: 2,
+        arc: 6,
+        episode: 122,
+        label: "Campaign 2 • Episode 122",
+        title: "Nothing Ventured, Nothing Gained"
+      },
+      
+      {
+        id: "c2e123",
+        campaign: 2,
+        arc: 6,
+        episode: 123,
+        label: "Campaign 2 • Episode 123",
+        title: "Fair-weather Faith"
+      },
+      
+      {
+        id: "c2e124",
+        campaign: 2,
+        arc: 6,
+        episode: 124,
+        label: "Campaign 2 • Episode 124",
+        title: "A Walk to Warmer Welcomes"
+      },
+      
+      {
+        id: "c2e125",
+        campaign: 2,
+        arc: 6,
+        episode: 125,
+        label: "Campaign 2 • Episode 125",
+        title: "The Neverending Day"
+      },
+      
+      {
+        id: "c2e126",
+        campaign: 2,
+        arc: 6,
+        episode: 126,
+        label: "Campaign 2 • Episode 126",
+        title: "Worth Fighting For"
+      },
+      
+      {
+        id: "c2e127",
+        campaign: 2,
+        arc: 6,
+        episode: 127,
+        label: "Campaign 2 • Episode 127",
+        title: "Sarsaparilla, Licorice, and Red Hot"
+      },
+      
+      {
+        id: "c2e128",
+        campaign: 2,
+        arc: 6,
+        episode: 128,
+        label: "Campaign 2 • Episode 128",
+        title: "Cat and Mouse"
+      },
+      
+      {
+        id: "c2e129",
+        campaign: 2,
+        arc: 6,
+        episode: 129,
+        label: "Campaign 2 • Episode 129",
+        title: "Between a Ball and a Hot Place"
+      },
+      
+      {
+        id: "c2e130",
+        campaign: 2,
+        arc: 6,
+        episode: 130,
+        label: "Campaign 2 • Episode 130",
+        title: "The Calm Before the Storm"
+      },
+      
+      {
+        id: "c2e131",
+        campaign: 2,
+        arc: 6,
+        episode: 131,
+        label: "Campaign 2 • Episode 131",
+        title: "Into the Eye"
+      },
+      
+      {
+        id: "c2e132",
+        campaign: 2,
+        arc: 6,
+        episode: 132,
+        label: "Campaign 2 • Episode 132",
+        title: "Aeor"
+      },
+      
+      {
+        id: "c2e133",
+        campaign: 2,
+        arc: 6,
+        episode: 133,
+        label: "Campaign 2 • Episode 133",
+        title: "Hunter and Hunted"
+      },
+      
+      {
+        id: "c2e134",
+        campaign: 2,
+        arc: 6,
+        episode: 134,
+        label: "Campaign 2 • Episode 134",
+        title: "The Streets of the Forgotten"
+      },
+      
+      {
+        id: "c2e135",
+        campaign: 2,
+        arc: 6,
+        episode: 135,
+        label: "Campaign 2 • Episode 135",
+        title: "The Genesis Ward"
+      },
+      
+      {
+        id: "c2e136",
+        campaign: 2,
+        arc: 6,
+        episode: 136,
+        label: "Campaign 2 • Episode 136",
+        title: "Hell or High Water"
+      },
+      
+      {
+        id: "c2e137",
+        campaign: 2,
+        arc: 6,
+        episode: 137,
+        label: "Campaign 2 • Episode 137",
+        title: "Welcome to Cognouza"
+      },
+      
+      {
+        id: "c2e138",
+        campaign: 2,
+        arc: 6,
+        episode: 138,
+        label: "Campaign 2 • Episode 138",
+        title: "Where There Is a Will..."
+      },
+      
+      {
+        id: "c2e139",
+        campaign: 2,
+        arc: 6,
+        episode: 139,
+        label: "Campaign 2 • Episode 139",
+        title: "Rebirth"
+      },
+      
+      {
+        id: "c2e140",
+        campaign: 2,
+        arc: 6,
+        episode: 140,
+        label: "Campaign 2 • Episode 140",
+        title: "Long May He Reign"
+      },
+      
+      {
+        id: "c2e141",
+        campaign: 2,
+        arc: 6,
+        episode: 141,
+        label: "Campaign 2 • Episode 141",
+        title: "Fond Farewells"
       }
 ];
 
