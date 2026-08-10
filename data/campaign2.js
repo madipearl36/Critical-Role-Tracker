347705e4edd160cec0e40bd2cc43b2c5fd39e82e
madipearl36 <madison.pearl0306@gmail.com>
2026-08-09 20:23:54 -0400

    Create campaign2.js

---
+++ data/campaign2.js
@@ -0,0 +1,230 @@
+const campaign2Episodes = [
+  {
+        id: "c2e1",
+        campaign: 2,
+        arc: 1,
+        episode: 1,
+        label: "Campaign 2 • Episode 1",
+        title: "Curious Beginnings"
+      },
+      
+      {
+        id: "c2e2",
+        campaign: 2,
+        arc: 1,
+        episode: 2,
+        label: "Campaign 2 • Episode 2",
+        title: "A Show of Scrutiny"
+      },
+      
+      {
+        id: "c2e3",
+        campaign: 2,
+        arc: 1,
+        episode: 3,
+        label: "Campaign 2 • Episode 3",
+        title: "The Midnight Chase"
+      },
+      
+      {
+        id: "c2e4",
+        campaign: 2,
+        arc: 1,
+        episode: 4,
+        label: "Campaign 2 • Episode 4",
+        title: "Disparate Pieces"
+      },
+      
+      {
+        id: "c2e5",
+        campaign: 2,
+        arc: 1,
+        episode: 5,
+        label: "Campaign 2 • Episode 5",
+        title: "The Open Road"
+      },
+      
+      {
+        id: "c2e6",
+        campaign: 2,
+        arc: 1,
+        episode: 6,
+        label: "Campaign 2 • Episode 6",
+        title: "The Howling Mines"
+      },
+      
+      {
+        id: "c2e7",
+        campaign: 2,
+        arc: 1,
+        episode: 7,
+        label: "Campaign 2 • Episode 7",
+        title: "Hush"
+      },
+      
+      {
+        id: "c2e8",
+        campaign: 2,
+        arc: 1,
+        episode: 8,
+        label: "Campaign 2 • Episode 8",
+        title: "The Gates of Zadash"
+      },
+      
+      {
+        id: "c2e9",
+        campaign: 2,
+        arc: 1,
+        episode: 9,
+        label: "Campaign 2 • Episode 9",
+        title: "Steam and Conversation"
+      },
+      
+      {
+        id: "c2e10",
+        campaign: 2,
+        arc: 1,
+        episode: 10,
+        label: "Campaign 2 • Episode 10",
+        title: "Waste and Webs"
+      },
+      
+      {
+        id: "c2e11",
+        campaign: 2,
+        arc: 1,
+        episode: 11,
+        label: "Campaign 2 • Episode 11",
+        title: "Zemnian Nights"
+      },
+      
+      {
+        id: "c2e12",
+        campaign: 2,
+        arc: 1,
+        episode: 12,
+        label: "Campaign 2 • Episode 12",
+        title: "Midnight Espionage"
+      },
+      
+      {
+        id: "c2e13",
+        campaign: 2,
+        arc: 1,
+        episode: 13,
+        label: "Campaign 2 • Episode 13",
+        title: "Lost & Found"
+      },
+      
+      {
+        id: "c2e14",
+        campaign: 2,
+        arc: 1,
+        episode: 14,
+        label: "Campaign 2 • Episode 14",
+        title: "Fleeting Memories"
+      },
+      
+      {
+        id: "c2e15",
+        campaign: 2,
+        arc: 1,
+        episode: 15,
+        label: "Campaign 2 • Episode 15",
+        title: "Where The River Goes"
+      },
+      
+      {
+        id: "c2e16",
+        campaign: 2,
+        arc: 1,
+        episode: 16,
+        label: "Campaign 2 • Episode 16",
+        title: "A Favor in Kind"
+      },
+      
+      {
+        id: "c2e17",
+        campaign: 2,
+        arc: 1,
+        episode: 17,
+        label: "Campaign 2 • Episode 17",
+        title: "Harvest Close"
+      },
+      
+      {
+        id: "c2e18",
+        campaign: 2,
+        arc: 1,
+        episode: 18,
+        label: "Campaign 2 • Episode 18",
+        title: "Whispers of War"
+      },
+      
+      {
+        id: "c2e19",
+        campaign: 2,
+        arc: 1,
+        episode: 19,
+        label: "Campaign 2 • Episode 19",
+        title: "The Gentleman's Path"
+      },
+      
+      {
+        id: "c2e20",
+        campaign: 2,
+        arc: 1,
+        episode: 20,
+        label: "Campaign 2 • Episode 20",
+        title: "Labenda Awaits"
+      },
+      
+      {
+        id: "c2e21",
+        campaign: 2,
+        arc: 1,
+        episode: 21,
+        label: "Campaign 2 • Episode 21",
+        title: "Stalker in the Swamp"
+      },
+      
+      {
+        id: "c2e22",
+        campaign: 2,
+        arc: 1,
+        episode: 22,
+        label: "Campaign 2 • Episode 22",
+        title: "Lost Treasures"
+      },
+      
+      {
+        id: "c2e23",
+        campaign: 2,
+        arc: 1,
+        episode: 23,
+        label: "Campaign 2 • Episode 23",
+        title: "Have Bird, Will Travel"
+      },
+      
+      {
+        id: "c2e24",
+        campaign: 2,
+        arc: 1,
+        episode: 24,
+        label: "Campaign 2 • Episode 24",
+        title: "The Hour of Honor"
+      },
+      
+      {
+        id: "c2e25",
+        campaign: 2,
+        arc: 1,
+        episode: 25,
+        label: "Campaign 2 • Episode 25",
+        title: "Divergent Paths"
+      }
+];
+
+const campaign2YoutubeLinks = {
+  
+};
