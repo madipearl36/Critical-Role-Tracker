b20f65509abf5c73664fe5ad881463c91705076c
madipearl36 <madison.pearl0306@gmail.com>
2026-08-11 14:23:16 -0400

    Added Arc 1

--- data/campaign3.js
+++ data/campaign3.js
@@ -1,7 +1,283 @@
 const campaign3Episodes = [
+
+  {
+    id: "c3e1",
+    campaign: 3,
+    arc: 1,
+    episode: 1,
+    label: "Campaign 3 • Episode 1",
+    title: "The Draw of Destiny",
+    duration: "3:58:24"
+  },
+  
+  {
+    id: "c3e2",
+    campaign: 3,
+    arc: 1,
+    episode: 2,
+    label: "Campaign 3 • Episode 2",
+    title: "Trial by Firelight",
+    duration: "4:07:39"
+  },
+  
+  {
+    id: "c3e3",
+    campaign: 3,
+    arc: 1,
+    episode: 3,
+    label: "Campaign 3 • Episode 3",
+    title: "The Trail and the Toll",
+    duration: "4:08:41"
+  },
+  
+  {
+    id: "c3e4",
+    campaign: 3,
+    arc: 1,
+    episode: 4,
+    label: "Campaign 3 • Episode 4",
+    title: "On the Trail of a Killer",
+    duration: "3:52:12"
+  },
+  
+  {
+    id: "c3e5",
+    campaign: 3,
+    arc: 1,
+    episode: 5,
+    label: "Campaign 3 • Episode 5",
+    title: "The Threat Between the Walls",
+    duration: "4:25:34"
+  },
+  
+  {
+    id: "c3e6",
+    campaign: 3,
+    arc: 1,
+    episode: 6,
+    label: "Campaign 3 • Episode 6",
+    title: "Growing Bonds and Teasing Threads",
+    duration: "3:54:57"
+  },
+  
+  {
+    id: "c3e7",
+    campaign: 3,
+    arc: 1,
+    episode: 7,
+    label: "Campaign 3 • Episode 7",
+    title: "Behind the Curtain",
+    duration: "3:49:37"
+  },
+  
+  {
+    id: "c3e8",
+    campaign: 3,
+    arc: 1,
+    episode: 8,
+    label: "Campaign 3 • Episode 8",
+    title: "A Woodworker's Quandary",
+    duration: "3:22:59"
+  },
+  
+  {
+    id: "c3e9",
+    campaign: 3,
+    arc: 1,
+    episode: 9,
+    label: "Campaign 3 • Episode 9",
+    title: "Thicker Grows the Meal and Plot",
+    duration: "4:14:35"
+  },
+  
+  {
+    id: "c3e10",
+    campaign: 3,
+    arc: 1,
+    episode: 10,
+    label: "Campaign 3 • Episode 10",
+    title: "Ghosts, Dates, and Darker Fates",
+    duration: "3:16:29"
+  },
+  
+  {
+    id: "c3e11",
+    campaign: 3,
+    arc: 1,
+    episode: 11,
+    label: "Campaign 3 • Episode 11",
+    title: "Chasing Nightmares",
+    duration: "4:52:18"
+  },
+  
+  {
+    id: "c3e12",
+    campaign: 3,
+    arc: 1,
+    episode: 12,
+    label: "Campaign 3 • Episode 12",
+    title: "Make It Fashion",
+    duration: "4:36:52"
+  },
+  
+  {
+    id: "c3e13",
+    campaign: 3,
+    arc: 1,
+    episode: 13,
+    label: "Campaign 3 • Episode 13",
+    title: "A Dance of Deception",
+    duration: "4:01:00"
+  },
+  
+  {
+    id: "c3e14",
+    campaign: 3,
+    arc: 1,
+    episode: 14,
+    label: "Campaign 3 • Episode 14",
+    title: "In Too Deep",
+    duration: "4:45:07"
+  },
+  
+  {
+    id: "c3e15",
+    campaign: 3,
+    arc: 1,
+    episode: 15,
+    label: "Campaign 3 • Episode 15",
+    title: "The Tunnels Below",
+    duration: "4:42:23"
+  },
+  
+  {
+    id: "c3e16",
+    campaign: 3,
+    arc: 1,
+    episode: 16,
+    label: "Campaign 3 • Episode 16",
+    title: "The Shade Mother",
+    duration: "4:12:11"
+  },
+  
+  {
+    id: "c3e17",
+    campaign: 3,
+    arc: 1,
+    episode: 17,
+    label: "Campaign 3 • Episode 17",
+    title: "Heart-to-Heartmoor",
+    duration: "4:11:29"
+  },
+  
+  {
+    id: "c3e18",
+    campaign: 3,
+    arc: 1,
+    episode: 18,
+    label: "Campaign 3 • Episode 18",
+    title: "A Hungry Jungle",
+    duration: "3:23:12"
+  },
+  
+  {
+    id: "c3e19",
+    campaign: 3,
+    arc: 1,
+    episode: 19,
+    label: "Campaign 3 • Episode 19",
+    title: "Omens Above",
+    duration: "3:55:11"
+  },
+  
+  {
+    id: "c3e20",
+    campaign: 3,
+    arc: 1,
+    episode: 20,
+    label: "Campaign 3 • Episode 20",
+    title: "Breaking and Entering...",
+    duration: "3:36:17"
+  },
+  
+  {
+    id: "c3e21",
+    campaign: 3,
+    arc: 1,
+    episode: 21,
+    label: "Campaign 3 • Episode 21",
+    title: "Fight at the Museum...",
+    duration: "3:47:43"
+  },
+  
+  {
+    id: "c3e22",
+    campaign: 3,
+    arc: 1,
+    episode: 22,
+    label: "Campaign 3 • Episode 22",
+    title: "Promise and Potential",
+    duration: "4:21:05"
+  },
+  
+  {
+    id: "c3e23",
+    campaign: 3,
+    arc: 1,
+    episode: 23,
+    label: "Campaign 3 • Episode 23",
+    title: "To The Skies",
+    duration: "4:14:52"
+  }
   
 ];
 
 const campaign3YoutubeLinks = {
+
+  1: "https://youtu.be/P8pLvV3FjPc",
+
+  2: "https://youtu.be/pWOivwWvtt0",
+
+  3: "https://youtu.be/POgfa65vi4k",
+
+  4: "https://youtu.be/gZ7u5NE_q4k",
+
+  5: "https://youtu.be/MDyYQqefj4M",
+
+  6: "https://youtu.be/iBeyMyMzQWg",
+
+  7: "https://youtu.be/mRyO6ijeLx8",
+
+  8: "https://youtu.be/cwbLO_649Vg",
+
+  9: "https://youtu.be/IdRagVTlrmo",
+
+  10: "https://youtu.be/Dvk70G5meeE",
+
+  11: "https://youtu.be/75SoOmgKuk4",
+
+  12: "https://youtu.be/h6AkvAj_hao",
+
+  13: "https://youtu.be/2CTz_OfneeM",
+
+  14: "https://youtu.be/-OCLsOUZN68",
+
+  15: "https://youtu.be/rMPp8dP1OC0",
+
+  16: "https://youtu.be/O6-NaqWorI0",
+
+  17: "https://youtu.be/c66mDHckyoM",
+
+  18: "https://youtu.be/f8WY6df598M",
+
+  19: "https://youtu.be/pPa_O6F5kqs",
+
+  20: "https://youtu.be/X9Bc-uuxCm4",
+
+  21: "https://youtu.be/4fK2wdBGCYo",
+
+  22: "https://youtu.be/_BfMZwPg4iM",
+
+  23: "https://youtu.be/rWZMRdLGBu0"
   
 };
